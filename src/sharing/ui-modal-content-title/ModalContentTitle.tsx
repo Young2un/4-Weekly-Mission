30673641dcd2acc8724f20eb52ab4f61eb6bfcca
영은\Hi <popduddms@gmail.com>
2024-04-08 19:02:40 +0900

    delete: 다시 하려고 다 지움

--- src/sharing/ui-modal-content-title/ModalContentTitle.tsx
+++
@@ -1,9 +0,0 @@
-import { PropsWithChildren } from "react";
-import styles from "./ModalContentTitle.module.scss";
-import classNames from "classnames/bind";
-
-const cx = classNames.bind(styles);
-
-export const ModalContentTitle = ({ children }: PropsWithChildren) => {
-  return <h2 className={cx("title")}>{children}</h2>;
-};
--- src/types/modalButton.ts
+++
@@ -1,8 +0,0 @@
-export interface ModalButton {
-  isOpen: boolean;
-  title: string;
-  buttonText: string;
-  onClick: () => void;
-  onCloseClick: () => void;
-  onKeyDown: () => void;
-}
